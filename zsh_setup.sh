5f754ddd328e2e51e1de4a10b9a0e23cbc64bc5c
Christoph Jesaja Heidelbach <christoph.jesaja.heidelbach@mailbox.org>
2021-10-23 16:50:23 +0200

    Add the unattended flag to omz installation promt

--- zsh_setup.sh
+++ zsh_setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update && sudo apt upgrade -y
 sudo apt install zsh -y
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" -y
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 mv ~/.zshrc ~/.zshrc.old
 mv ~/.p10k.zsh ~/.p10k.zsh.old
